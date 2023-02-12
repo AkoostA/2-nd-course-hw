ce20bf0c9b78a5c31aa52354508e15fcc23c215d
Ilia Abramov <akoostakai@gmail.com>
2023-02-12 11:40:03 +0300

    hw-7

--- Верстка сайта/script.js
+++ Верстка сайта/script.js
@@ -20,37 +20,22 @@ const seasons = () => {
     return;
 }
 
-// const seasons = () => {
-//     const date = Number(prompt("Введите номер месяца (от 1 до 12) и вы узнаете к какому времени года он относится"));
+const words = () => {
+    let words = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    words = words.sort(() => Math.random() - 0.5);
 
-//     switch (date) {
-//         case 12:
-//         case 1:
-//         case 2:
-//             console.log("Зима");
-//             alert("Зима");
-//             break;
-//         case 3:
-//         case 4:
-//         case 5:
-//             console.log("Весна");
-//             alert("Весна");
-//             break;
-//         case 6:
-//         case 7:
-//         case 8:
-//             console.log("Лето");
-//             alert("Лето");
-//             break;
-//         case 9:
-//         case 10:
-//         case 11:
-//             console.log("Осень");
-//             alert("Осень");
-//             break;
-//         default:
-//             console.log("Вы ввели не корректное значение, попробуйте еще раз");
-//             alert("Вы ввели не корректное значение, попробуйте еще раз");
-//             break;
-//     }
-// }
+
+    words.forEach(words => alert(words));
+    const firstWord = prompt("Какое было первое слово?");
+    const lastWord = prompt("Какое было последнее слово?");
+
+    if (firstWord.toLowerCase() === words[0].toLowerCase() && lastWord.toLowerCase() === words[6].toLowerCase()) {
+        alert("Поздравляю, вы угадали оба слова!");
+        return;
+    } else if (firstWord.toLowerCase() === words[0].toLowerCase() || lastWord.toLowerCase() === words[6].toLowerCase()) {
+        alert("Вы были близки к победе!");
+        return;
+    } else {
+        alert("Вы ответили неверно.");
+    }
+}
